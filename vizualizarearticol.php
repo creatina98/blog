3eecee52e194133d91ef19abfa05a8b446da8cfb
PreotulKazoo <60981909+PreotulKazoo@users.noreply.github.com>
2020-07-08 18:30:00 +0300

    Update vizualizarearticol.php

--- vizualizarearticol.php
+++ vizualizarearticol.php
@@ -28,7 +28,8 @@
                </div>
          
             <img class="headimg" src="./img/creativity.jpg">
-            <p><?php echo  $item['continut_articol'];?> </p>
+
+            <div><?php echo  $item['continut_articol'];?> </div>
 
 
                <?php
@@ -41,8 +42,24 @@
                     <button id="likeButton" style="font-size:24px"><i class="fa fa-heart fe-pulse-w-pause" style="font-size: 30px;"></i></button>
                 </div>
                 <hr>
-                <h2>Lista comentarii</h2>
+                <h2 id="titluLista">Lista comentarii</h2>
+               <?php
+               $array2 = array();
+               $sql2 =  "SELECT * FROM comentariu,articol WHERE comentariu.id_articol=articol.id_articol and articol.id_articol=". $_GET['id_art'];
+               $result2 = mysqli_query($conn, $sql2);
+               $resultCheck2 = mysqli_num_rows($result2);
+
+               if ($resultCheck2 > 0) {
+                   while ($row2 = mysqli_fetch_assoc($result2)) {
+                       $array2[] = $row2;
+                   }
+               }else { ?> <style> #titluLista{display: none;} .listaComentarii{display: none}</style> <?php }
+               ?>
+
                 <div class="listaComentarii">
+                    <?php
+                    foreach ($array2 as $item2) {
+                        ?>
                     <div class="boxImgScrisAutor">
                         <img src="./img/icon9.png" alt="icon" width="55px" height="58px">
                         <div class="autorCometariu">
@@ -50,8 +67,9 @@
                             <p>11 iun. 2020, 20:32</p>
                         </div>
                    </div>
-                   <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.
-                    The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using</p>
+
+                   <p><?php echo  $item2['comentariu'];?></p>
+               <?php }  ?>
                 </div>
                 
                 <div class="inputAddComentarii">
@@ -76,7 +94,7 @@
                         <h4 class="headrecent">Articole recente</h4>
                         <?php
                         $array1 = array();
-                        $sql1 = "SELECT * FROM articol limit 5 ;";
+                        $sql1 = "SELECT * FROM articol limit 6 ;";
                         $result1 = mysqli_query($conn, $sql1);
                         $resultCheck1 = mysqli_num_rows($result1);
 
@@ -88,23 +106,31 @@
                         <?php
                         foreach ($array1 as $item1) {
                         ?>
-                        <p><?php echo  $item1['titlul_articolului'];?></p>
-                          <hr>
+                        <a style="text-decoration: none;color:#1F2732" href="vizualizarearticol.php?id_art=<?php echo $item1["id_articol"];?>"><p><?php echo  $item1['titlul_articolului'];?></p></a>
+                            <hr>
                         <?php } ?>
 
 
                     </div>
                     <div>
-                        <h4 class="headalte">Alte sectiuni</h4>
-                        <p>Ce este lumea? Cum sa ne integram... Multa fericire si baniiiiii</p>
-                        <hr>
-                        <p>Ce este lumea? Cum sa ne integram... Multa fericire si baniiiiii</p>
-                        <hr>
-                        <p>Ce este lumea? Cum sa ne integram... Multa fericire si baniiiiii</p>
-                        <hr>
-                        <p>Ce este lumea? Cum sa ne integram... Multa fericire si baniiiiii</p>
-                        <hr>
-                        <p>Ce este lumea? Cum sa ne integram... Multa fericire si baniiiiii</p>
+                        <h4 class="headalte">Articol random</h4>
+                        <?php
+                        $array2 = array();
+                        $sql2 = "SELECT * FROM articol  order by rand() limit 1 ;";
+                        $result2 = mysqli_query($conn, $sql2);
+                        $resultCheck2 = mysqli_num_rows($result2);
+
+                        if ($resultCheck2 > 0) {
+                            while ($row2 = mysqli_fetch_assoc($result2)) {
+                                $array2[] = $row2;
+                            }
+                        } ?>
+                        <?php
+                        foreach ($array2 as $item2) {
+                            ?>
+                            <a style="text-decoration: none;color:#1F2732" href="vizualizarearticol.php?id_art=<?php echo $item2["id_articol"];?>"><p><?php echo  $item2['titlul_articolului'];?></p></a>
+                            <hr>
+                        <?php } ?>
                     </div>
                </div>
            </div>
