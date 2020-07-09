566fb7233b63356fa724ee96f677fb973472ccd1
creatina98 <34490820+creatina98@users.noreply.github.com>
2020-07-09 19:34:29 +0300

    dashboard

--- dashboard/admin.php
+++ dashboard/admin.php
@@ -31,7 +31,7 @@
             <textarea name="content" id="content">
                 <?php echo !empty($_POST['content'])?$_POST['content']:''; ?>
             </textarea>
-            <div class="buttons">
+            <div class="buttons" style="margin-bottom: 100px">
                 <div class="left">
                     <div class="coloana">
                         <h4>Imagine Articol</h4>
@@ -62,22 +62,40 @@
                 <div style="border-left:1px solid rgb(114, 114, 114);height:200px; margin-top: 50px;"></div>
 
                 <div class="right">
-                     <button name="submit" type="submit">Adauga Articol</button>
+                    <button name="submit" type="submit">Previzualizare</button>
+                     <button name="adaugaArticol" type="submit">Adauga Articol</button>
                 </div>
             </div>
-            <div class="previzualizareArticol">
+<!--            <div class="previzualizareArticol">-->
+<!--                --><?php
+//                if(!empty($_POST['content'])){
+//                    ?>
+<!--                    <div class="output" style="font-size:20px;line-height: 0.9;">-->
+<!--                        <h1>Rezultat</h1>-->
+<!--                        --><?php //echo $_POST['content']; ?>
+<!--                    </div>-->
+<!--                    --><?php
+//                }
+//                ?>
+<!--            </div>-->
+
+
+            <div class="row mt-5" style="font-family: 'Lato', sans-serif">
+            <div class="col-8">
+                <div class="titlulDataArticol">
+                    <h1 style="font-family: Rockwell; font-weight: 300; margin-bottom: 0; font-size: 50px"> Acesta este titlul articolului add de me</h1>
+                    <p>11 iun. 2020, 20:32</p>
+                </div>
+
+                <img class="headimg" src="../img/creativity.jpg">
                 <?php
                 if(!empty($_POST['content'])){
-                    ?>
-                    <div class="output" style="font-size:20px;line-height: 0.9;">
-                        <h1>Rezultat</h1>
-                        <?php echo $_POST['content']; ?>
-                    </div>
+                ?>
+                <div style=""> <?php echo $_POST['content']; ?></div>
                     <?php
                 }
                 ?>
             </div>
-        </div>
    </div> 
    <script src="../ckeditor/ckeditor.js"></script>
    <script>
---
+++ dashboard/managerPostari.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./styleManagerPostari.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <div class="navbar">
+            <h1>Nume prenume editor</h1>
+            <ul>
+                <li>Editor postari</li>
+                <li>Manager postari</li>
+            </ul>
+            <div class="boxContinut">
+                <p>I'M A BLOGGER what's your superpower?</p>
+            </div>
+        </div>
+        <div class="postari">
+            <div class="boxArticol">
+                <div class="boxContinutArticol">
+                    <h3>Titlul articolului</h3>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                     Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                      when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                       It has survived not only five centuries, but also the leap into electronic typesetting, remaining
+                        essentially unchanged.</p>
+                        <button>Update</button>
+                </div>
+            </div>            
+        </div>
+    </div>
+</body>
+</html>
